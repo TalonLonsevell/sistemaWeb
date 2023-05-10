45d92ecc98da62280001f093490e2d20d1405d5a
Enzo Castilho <enzocastilho@hotmail.com>
2023-05-10 18:51:22 -0300

    Cálculo da média

--- Principal.java
+++ Principal.java
@@ -2,5 +2,8 @@ class Principal {
     public static void main(String[] args) {
         System.out.println("----Sistema Web-----");
         double a = 10;
+        double b = 8;
+        double media = (a + b) / 2;
+        System.out.println("Média: " + media);
     }
 }
