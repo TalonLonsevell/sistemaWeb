1e823d0bb626efeb46af3ba60ed2c2de02a3d952
Enzo Castilho <enzocastilho@hotmail.com>
2023-05-10 18:54:55 -0300

    Novo cálculo da média

--- Principal.java
+++ Principal.java
@@ -3,7 +3,8 @@ class Principal {
         System.out.println("----Sistema Web-----");
         double a = 10;
         double b = 8;
-        double media = (a + b) / 2;
+        double c = 10;
+        double media = (a + b + c) / 3;
         System.out.println("Média: " + media);
     }
 }
